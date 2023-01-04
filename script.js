daab43c7a0998d6c0ac47e942913ff7a04befd3b
Natalius <nataliusgabriel@gmail.com>
2023-01-04 17:21:31 +0700

    made button available for live demos

--- script.js
+++ script.js
@@ -6,14 +6,19 @@ function httpGet(theUrl)
     return xmlHttp.responseText;
 }
 
-const repos = JSON.parse(httpGet("https://api.github.com/users/natalius-dev/repos?sort=created"));
+const id = "d62acd24a757233d43b0";
+const secret = "5cf44622dfe3109ab82dbc57cd6bbc4fcf62d1c6";
+
+const repos = JSON.parse(httpGet("https://api.github.com/users/natalius-dev/repos?sort=created&client_id="+id+"&client_secret="+secret));
 const container = document.getElementById("container");
 
 for(let i = 0; i < repos.length; i++) {
+    const repo_name = repos[i.toString()].name;
+
     var card = document.createElement("div");
     card.classList.add("card","flex-grow-1","m-2");
 
-    const contents = JSON.parse(httpGet("https://api.github.com/repos/natalius-dev/"+repos[i.toString()].name+"/contents/"))
+    const contents = JSON.parse(httpGet("https://api.github.com/repos/natalius-dev/"+repo_name+"/contents/?client_id="+id+"&client_secret="+secret))
     let image_file = "thumbnail 404.svg";
     const url = repos[i.toString()].html_url;
     for(let i = 0; i < contents.length; i++) {
@@ -26,7 +31,7 @@ for(let i = 0; i < repos.length; i++) {
     let img = document.createElement("img");
     img.src = image_file;
     img.classList.add("card-img-top","thumbnail");
-    img.alt = repos[i.toString()].name;
+    img.alt = repo_name;
     card.appendChild(img);
 
     let card_body = document.createElement("div");
@@ -34,20 +39,28 @@ for(let i = 0; i < repos.length; i++) {
 
     let card_title = document.createElement("h5");
     card_title.classList.add("card-title");
-    card_title.innerHTML = repos[i.toString()].name;
+    let card_title_a = document.createElement("a");
+    card_title_a.innerHTML = repo_name;
+    card_title_a.href = url;
+    card_title_a.target = "_blank";
+    card_title.appendChild(card_title_a);
 
     let card_text = document.createElement("p");
     card_text.classList.add("card-text","text-muted");
     card_text.innerHTML = repos[i.toString()].description;
 
-    let button = document.createElement("button");
-    button.classList.add("py-2","px-3");
-    button.onclick = function(){setTimeout(function(){window.open(repos[i.toString()].html_url);},250)};
-    button.innerHTML = "Visit Repository";
-    
+    const deployments = JSON.parse(httpGet("https://api.github.com/repos/natalius-dev/"+repo_name+"/deployments?client_id="+id+"&client_secret="+secret))
     card_body.appendChild(card_title);
     card_body.appendChild(card_text);
-    card_body.appendChild(button);
+    if(deployments.length > 0) {
+        if(JSON.parse(httpGet(deployments["0"].statuses_url))["0"].state === "success"){
+            let button = document.createElement("button");
+            button.classList.add("py-2","px-3");
+            button.onclick = function(){setTimeout(function(){window.open("https://github.natalius.cc/"+repo_name);},250)};
+            button.innerHTML = "Visit Live Demo";
+            card_body.appendChild(button);
+        }
+    }
 
     card.appendChild(card_body);
 
